a09d6ec5adbbbf73e89a758ab34ac19b118791d9
Alan Kay <alanajkay@gmail.com>
2021-05-20 20:44:46 -0500

    Working via direct CLI with no prompts

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# adguard-home-batch-blocklist-manager
+# adguard-home-batch-list-manager
 
-Add a bunch of lists to your AdGuard Home blocklists
+Add a bunch of lists to your AdGuard Home lists, including blocklists and whitelists
 
 ## Setup
 
@@ -11,6 +11,31 @@ Add a bunch of lists to your AdGuard Home blocklists
     pip install -r requirements.txt
     ```
 
+## Running
+
+Run the project, and follow the prompts:
+
+```shell
+python adguard_list_manager.py
+```
+
+Alternatively, you can pass CLI args directly. For example, here is how you can clear your blacklists:
+
+```shell
+python adguard_list_manager.py --list-type blacklist --list-action clear --host YourAdGuardIPAddressHere --port YourAdGuardPortHere --username YourAdGuardUsernameHere --password YourAdGuardPasswordHere
+```
+
+And here is how you can add the ticket lists to your blacklists:
+
+```shell
+python adguard_list_manager.py --list-type blacklist --list-action add --blacklist-source firebog_ticked --host YourAdGuardIPAddressHere --port YourAdGuardPortHere --username YourAdGuardUsernameHere --password YourAdGuardPasswordHere
+```
+
+## Current Limitations
+
+- Assumes you are not using HTTPS. It may take some refactoring to get this working with HTTPS
+- Doesn't provide a way to give a custom list.
+
 ## Credits
 
-Thanks to the user who posted on [reddit](https://pastebin.com/i1d4xNAY) and had a [pastebin](https://pastebin.com/i1d4xNAY) link
+Thanks to the user who posted on [reddit](https://pastebin.com/i1d4xNAY) and had a [pastebin](https://pastebin.com/i1d4xNAY) to get me started
--- adguard_list_manager.py
+++ adguard_list_manager.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Optional
 
 import requests
 import typer
@@ -39,11 +40,52 @@ class BlacklistSource(str, Enum):
         return next(item["value"] for item in mappings if item["key"] == self)
 
 
+class WhitelistSource(str, Enum):
+    anudeepND_safe = "anudeepND_safe"
+    anudeepND_optional = "anudeepND_optional"
+    anudeepND_referral = "anudeepND_referral"
+    anudeepND_safe_plus_optional = "anudeepND_safe_plus_optional"
+
+    @property
+    def url(self):
+        # Yes, this is horrible. Typer uses the value, not the key, so this makes the user experience better"
+        mappings = [
+            {
+                "key": WhitelistSource.anudeepND_safe,
+                "value": [
+                    "https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt"
+                ],
+            },
+            {
+                "key": WhitelistSource.anudeepND_optional,
+                "value": [
+                    "https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/optional-list.txt"
+                ],
+            },
+            {
+                "key": WhitelistSource.anudeepND_referral,
+                "value": [
+                    "https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/referral-sites.txt"
+                ],
+            },
+            {
+                "key": WhitelistSource.anudeepND_safe_plus_optional,
+                "value": [
+                    "https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/optional-list.txt",
+                    "https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt",
+                ],
+            },
+        ]
+        return next(item["value"] for item in mappings if item["key"] == self)
+
+
 class AdguardListManager:
     def main(
         self,
         list_type: ListType = typer.Option(..., prompt=True),
         list_action: ListAction = typer.Option(..., prompt=True),
+        blacklist_source: BlacklistSource = typer.Option(None),
+        whitelist_source: WhitelistSource = typer.Option(None),
         host: str = typer.Option(..., prompt=True, help="Example: 192.168.1.5"),
         port: int = typer.Option(..., prompt=True, help="Example: 80"),
         username: str = typer.Option(
@@ -61,17 +103,24 @@ class AdguardListManager:
 
         try:
             if list_type == ListType.blacklist and list_action == ListAction.add:
-                typer.run(self.add_blacklists)
+                if blacklist_source is not None:
+                    self.add_blacklists_from_url(list_source_url=blacklist_source.url)
+                else:
+                    typer.run(self.add_blacklists)
             elif list_type == ListType.blacklist and list_action == ListAction.clear:
                 self.clear_list()
             elif list_type == ListType.whitelist and list_action == ListAction.add:
-                self.add_to_list(whitelist=True)
+                if whitelist_source is not None:
+                    self.add_to_list(whitelist_source.url, whitelist=True)
+                else:
+                    typer.run(self.add_whitelists)
             elif list_type == ListType.whitelist and list_action == ListAction.clear:
                 self.clear_list(whitelist=True)
             else:
                 print("Unknown or unimplemented path. Sorry!")
         except Exception as e:
-            print(f"Something bad happened! See the error for more details: {e}")
+            print(f"Something bad happened! See the error for more details:")
+            print(e)
         finally:
             self.session.close()
 
@@ -82,11 +131,20 @@ class AdguardListManager:
         )
         response.raise_for_status()
 
+    def add_blacklists_from_url(self, list_source_url: str):
+        urls = self.get_lists_from_url(list_source_url)
+        self.add_to_list(urls)
+
     def add_blacklists(
         self, list_source: BlacklistSource = typer.Option(..., prompt=True)
     ):
         urls = self.get_lists_from_url(list_source.url)
         self.add_to_list(urls)
+
+    def add_whitelists(
+        self, list_source: WhitelistSource = typer.Option(..., prompt=True)
+    ):
+        self.add_to_list(list_source.url, whitelist=True)
 
     def get_lists_from_url(self, url: str):
         response = requests.get(url)
@@ -107,12 +165,13 @@ class AdguardListManager:
         response.raise_for_status()
 
         filters = response.json().get("filters", [])
-        for filter in filters:
-            response = self.session.post(
-                f"{self.host}/control/filtering/remove_url",
-                json={"url": filter["url"], "whitelist": whitelist},
-            )
-            response.raise_for_status()
+        if filters is not None:
+            for filter in filters:
+                response = self.session.post(
+                    f"{self.host}/control/filtering/remove_url",
+                    json={"url": filter["url"], "whitelist": whitelist},
+                )
+                response.raise_for_status()
 
 
 if __name__ == "__main__":
