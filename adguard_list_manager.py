524cd9c48edfc4ac5aa547fb199b30978e67a856
Alan Kay <alanajkay@gmail.com>
2021-05-20 19:51:31 -0500

    Added looking up of lists

--- adguard_list_manager.py
+++ adguard_list_manager.py
@@ -14,6 +14,31 @@ class ListAction(str, Enum):
     clear = "clear"
 
 
+class BlacklistSource(str, Enum):
+    firebog_ticked = "firebog_ticked"
+    firebog_noncrossed = "firebog_noncrossed"
+    firebog_all = "firebog_all"
+
+    @property
+    def url(self):
+        # Yes, this is horrible. Typer uses the value, not the key, so this makes the user experience better"
+        mappings = [
+            {
+                "key": BlacklistSource.firebog_ticked,
+                "value": "https://v.firebog.net/hosts/lists.php?type=tick",
+            },
+            {
+                "key": BlacklistSource.firebog_noncrossed,
+                "value": "https://v.firebog.net/hosts/lists.php?type=nocross",
+            },
+            {
+                "key": BlacklistSource.firebog_all,
+                "value": "https://v.firebog.net/hosts/lists.php?type=all",
+            },
+        ]
+        return next(item["value"] for item in mappings if item["key"] == self)
+
+
 class AdguardListManager:
     def main(
         self,
@@ -30,13 +55,13 @@ class AdguardListManager:
             hide_input=True,
             help="Password to log into the AdGuard UI",
         ),
-    ):
+    ) -> None:
         self.host = f"http://{host}:{port}"
         self.get_logged_in_session(username, password)
 
         try:
             if list_type == ListType.blacklist and list_action == ListAction.add:
-                self.add_to_list()
+                typer.run(self.add_blacklists)
             elif list_type == ListType.blacklist and list_action == ListAction.clear:
                 self.clear_list()
             elif list_type == ListType.whitelist and list_action == ListAction.add:
@@ -57,40 +82,19 @@ class AdguardListManager:
         )
         response.raise_for_status()
 
-    def add_to_list(self, whitelist: bool = False):
-        urls = [
-            "https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADhosts.txt",
-            "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts",
-            "https://v.firebog.net/hosts/static/w3kbl.txt",
-            "https://adaway.org/hosts.txt",
-            "https://v.firebog.net/hosts/AdguardDNS.txt",
-            "https://v.firebog.net/hosts/Admiral.txt",
-            "https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt",
-            "https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt",
-            "https://v.firebog.net/hosts/Easylist.txt",
-            "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext",
-            "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/UncheckyAds/hosts",
-            "https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts",
-            "https://v.firebog.net/hosts/Easyprivacy.txt",
-            "https://v.firebog.net/hosts/Prigent-Ads.txt",
-            "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts",
-            "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt",
-            "https://hostfiles.frogeye.fr/firstparty-trackers-hosts.txt",
-            "https://raw.githubusercontent.com/Kees1958/W3C_annual_most_used_survey_blocklist/master/TOP_EU_US_Ads_Trackers_HOST",
-            "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt",
-            "https://osint.digitalside.it/Threat-Intel/lists/latestdomains.txt",
-            "https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt",
-            "https://v.firebog.net/hosts/Prigent-Crypto.txt",
-            "https://bitbucket.org/ethanr/dns-blacklists/raw/8575c9f96e5b4a1308f2f12394abd86d0927a4a0/bad_lists/Mandiant_APT1_Report_Appendix_D.txt",
-            "https://phishing.army/download/phishing_army_blocklist_extended.txt",
-            "https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-malware.txt",
-            "https://v.firebog.net/hosts/Shalla-mal.txt",
-            "https://raw.githubusercontent.com/Spam404/lists/master/main-blacklist.txt",
-            "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts",
-            "https://urlhaus.abuse.ch/downloads/hostfile/",
-            "https://zerodot1.gitlab.io/CoinBlockerLists/hosts_browser",
-        ]
+    def add_blacklists(
+        self, list_source: BlacklistSource = typer.Option(..., prompt=True)
+    ):
+        urls = self.get_lists_from_url(list_source.url)
+        self.add_to_list(urls)
 
+    def get_lists_from_url(self, url: str):
+        response = requests.get(url)
+        response.raise_for_status()
+        urls = list(set(response.text.split("\n")))
+        return [x for x in urls if x]
+
+    def add_to_list(self, urls: list, whitelist: bool = False) -> None:
         for url in urls:
             response = self.session.post(
                 f"{self.host}/control/filtering/add_url",
@@ -98,7 +102,7 @@ class AdguardListManager:
             )
             response.raise_for_status()
 
-    def clear_list(self, whitelist: bool):
+    def clear_list(self, whitelist: bool = False) -> None:
         response = self.session.get(f"{self.host}/control/filtering/status")
         response.raise_for_status()
 
